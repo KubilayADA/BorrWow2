1cb9ca50c265c504c29f62bcb17b2158dc493aba
Kubilay ADA <kubilayada.26@gmail.com>
2025-01-28 12:47:42 +0100

    db/index.js / connection to atlas fixed

--- backend_borrWow/db/index.js
+++ backend_borrWow/db/index.js
@@ -1,19 +1,15 @@
-// ℹ️ package responsible to make the connection with mongodb
-// https://www.npmjs.com/package/mongoose
 const mongoose = require("mongoose");
 
-
-const MONGO_URI =
-  process.env.MONGODB_URI || `mongodb://admin:${process.env.MONGO_PASSWORD}@127.0.0.1:27017/borrWowAPI?authSource=admin`;
-
-
+// .env dosyasından MONGODB_URI değişkenini al
+const MONGO_URI = process.env.MONGO_URI;
 
 const withDB = async (serverListener) => {
   try {
-    const x = await mongoose.connect(MONGO_URI);
-    console.log(
-      `Connected to Mongo! Database name: "${x.connections[0].name}"`
-    );
+    const x = await mongoose.connect(MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`);
     if (typeof serverListener === "function") {
       serverListener();
     }
--- backend_borrWow/server.js
+++ backend_borrWow/server.js
@@ -1,5 +1,8 @@
+require("dotenv").config();
+
 const app = require('./app');
 const withDB = require('./db');
+
 
 // ℹ️ Sets the PORT for our app to have access to it. If no env has been set, we hard code it to 5005
 const PORT = process.env.PORT || 5005;
