b46c577452f359045e716654e32ea2729bb92cf7
Kubilay ADA <kubilayada.26@gmail.com>
2025-02-07 17:18:28 +0100

    Double check of user routes

--- backend_borrWow/routes/user.routes.js
+++ backend_borrWow/routes/user.routes.js
@@ -3,17 +3,7 @@ const router = express.Router();
 const User = require("../models/User.model.js");
 const { isAuthenticated } = require("../middlewares/auth.middleware");
 
-/* // Create a new user is now in auth 
-router.post("/", async (req, res) => {
-  try {
-    const user = await User.create(req.body);
-    res.status(201).json(user);
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-}); */
 
-// Get all users
 router.get("/", async (req, res) => {
   try {
     const users = await User.find();
@@ -23,7 +13,7 @@ router.get("/", async (req, res) => {
   }
 });
 
-//Get user by ID
+
 
 router.get("/:id", async (req, res) => {
   try {
