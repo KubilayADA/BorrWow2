f039be8588c7e2b9993b00d9f9cc2919613f26ef
Kubilay ADA <kubilayada.26@gmail.com>
2025-03-12 18:07:54 +0100

    deply problem due to routing

--- backend_borrWow/routes/index.routes.js
+++ backend_borrWow/routes/index.routes.js
@@ -1,23 +1,15 @@
-const express = require("express");
-const app = express();
-const itemRouter = require("./routes/item.routes"); // Your existing API router
+const router = require("express").Router();
+const userRoutes = require("./user.routes");
+const itemRoutes = require("./item.routes");
+const borrowRequestRoutes = require("./borrowRequest.routes");
 
-
-app.use(cors());
-app.use(express.json());
-
-
-app.get("/", (req, res) => {
-  res.json({ message: "Backend is running!" });
+// these are all the api routes e.g http://localhost:5005/api/items
+router.get("/", (req, res) => {
+  res.json("Hello from the /api route!");
 });
 
+router.use("/users", userRoutes);
+router.use("/items", itemRoutes);
+router.use("/borrowrequests", borrowRequestRoutes);
 
-app.use("/items", itemRouter);
-
-
-app.use((err, req, res, next) => {
-  console.error(err);
-  res.status(500).json({ message: "Internal server error" });
-});
-
-module.exports = app;
+module.exports = router;
