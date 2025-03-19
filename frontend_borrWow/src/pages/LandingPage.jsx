22b182cb94a058bf7cc5e4521686c0049b051f0f
Kubilay ADA <kubilayada.26@gmail.com>
2025-03-19 22:16:21 +0100

    make invite friends box on landing page invisible when user logged in since its direct you to login page

--- frontend_borrWow/src/pages/LandingPage.jsx
+++ frontend_borrWow/src/pages/LandingPage.jsx
@@ -1,37 +1,39 @@
-import React from "react";
+import React, { useContext } from "react"; 
 import { Container, Text, Center } from "@mantine/core";
 import { Link } from "react-router-dom";
-import logo from "../assets/images/logo.png"; // Import the image
-import styles from "../styles/LandingPage.module.css"; // Import CSS module
+import logo from "../assets/images/logo.png";
+import styles from "../styles/LandingPage.module.css";
 import cash from "../assets/images/cash.png";
+import { SessionContext } from "../contexts/SessionContext";
 
 function LandingPage() {
+  const { isAuthenticated } = useContext(SessionContext);
+  
   return (
     <Container className={styles.container}>
       <Center className={styles.center}>
-        {/* Logo */}
         <img src={logo} alt="Logo" className={styles.logo} />
-
         <div>
           <Text align="center" mt="md">
             <strong>Welcome to borrWow.</strong> A local platform to share,
             help, and lend more than a hand. Join our community now!
           </Text>
 
-          {/* Invite Box Section */}
-          <div className={styles.box}>
-            <img src={cash} alt="Invite" className={styles.cash} />
-            <div className={styles.boxText}>
-              <p className={styles.invite}>
-                <strong>Invite friends now</strong> and get €30 BorrWower Cash
-                for each friend that joins! Each friend also gets 1 month free.
-                It’s a win-win!
-              </p>
-              <Link to="/invitefriends">
-                <button className={styles.button}>Find out more</button>
-              </Link>
+          {!isAuthenticated && (
+            <div className={styles.box}>
+              <img src={cash} alt="Invite" className={styles.cash} />
+              <div className={styles.boxText}>
+                <p className={styles.invite}>
+                  <strong>Invite friends now</strong> and get €30 BorrWower Cash
+                  for each friend that joins! Each friend also gets 1 month free.
+                  It’s a win-win!
+                </p>
+                <Link to="/invitefriends">
+                  <button className={styles.button}>Find out more</button>
+                </Link>
+              </div>
             </div>
-          </div>
+          )}
         </div>
       </Center>
     </Container>
