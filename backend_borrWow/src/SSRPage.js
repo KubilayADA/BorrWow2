d64262db6e17c40d0a60eb2d6203a10d0bf6b6f5
Kubilay ADA <kubilayada.26@gmail.com>
2025-04-15 00:36:07 +0200

    navbar on ssr page was not functional causes many issues thats why i try to implement a back button based on the server-history

--- backend_borrWow/src/SSRPage.js
+++ backend_borrWow/src/SSRPage.js
@@ -7,28 +7,11 @@ function SSRPage() {
       React.createElement("link", {
         key: 2,
         rel: "stylesheet",
-        href: "/api/ssr-page-styles",
+        href: "/api/ssr-page-styles", 
       }),
     ]),
     React.createElement("body", { key: 2 }, [
-      React.createElement("div", { className: "navbar", key: 1 }, [
-        React.createElement(
-          "a",
-          { href: "/", key: 1 },
-          "Home"
-        ),
-        React.createElement(
-          "a",
-          { href: "/about", key: 2 },
-          "About"
-        ),
-        React.createElement(
-          "a",
-          { href: "/invitefriends", key: 3 },
-          "Invite Friends"
-        ),
-      ]),
-      React.createElement("div", { className: "container", key: 2 }, [
+      React.createElement("div", { className: "container", key: 1 }, [
         React.createElement(
           "h1",
           { key: 1 },
@@ -43,6 +26,24 @@ function SSRPage() {
           "p",
           { key: 3 },
           "Rendered on the server for SE_19!"
+        ),
+        
+        React.createElement(
+          "button",
+          {
+            key: 4,
+            onClick: "history.back()", 
+            style: {
+              marginTop: "20px",
+              padding: "10px 20px",
+              backgroundColor: "#224eff",
+              color: "#fff",
+              border: "none",
+              borderRadius: "5px",
+              cursor: "pointer",
+            },
+          },
+          "Go Back"
         ),
       ]),
     ]),
