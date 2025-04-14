fa2a1993d58507dfa8fe67c38cb079501d42d8a6
Kubilay ADA <kubilayada.26@gmail.com>
2025-04-15 00:28:40 +0200

    styled ssr page

--- backend_borrWow/src/SSRPage.js
+++ backend_borrWow/src/SSRPage.js
@@ -1,9 +1,51 @@
 const React = require("react");
 
 function SSRPage() {
-  return React.createElement("div", null, [
-    React.createElement("h1", { key: 1 }, "This is an SSR WELCOMING PAGE, this page only made for showing knowledge of SSR"),
-    React.createElement("p", { key: 2 }, "Rendered on the server for SE_19!"),
+  return React.createElement("html", null, [
+    React.createElement("head", { key: 1 }, [
+      React.createElement("title", { key: 1 }, "SSR Welcoming Page"),
+      React.createElement("link", {
+        key: 2,
+        rel: "stylesheet",
+        href: "/api/ssr-page-styles",
+      }),
+    ]),
+    React.createElement("body", { key: 2 }, [
+      React.createElement("div", { className: "navbar", key: 1 }, [
+        React.createElement(
+          "a",
+          { href: "/", key: 1 },
+          "Home"
+        ),
+        React.createElement(
+          "a",
+          { href: "/about", key: 2 },
+          "About"
+        ),
+        React.createElement(
+          "a",
+          { href: "/invitefriends", key: 3 },
+          "Invite Friends"
+        ),
+      ]),
+      React.createElement("div", { className: "container", key: 2 }, [
+        React.createElement(
+          "h1",
+          { key: 1 },
+          "This is an SSR WELCOMING PAGE"
+        ),
+        React.createElement(
+          "p",
+          { key: 2 },
+          "This page is only made for showing knowledge of SSR."
+        ),
+        React.createElement(
+          "p",
+          { key: 3 },
+          "Rendered on the server for SE_19!"
+        ),
+      ]),
+    ]),
   ]);
 }
 
