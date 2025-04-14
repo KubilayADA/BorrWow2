2f82d8bdb122acf162b0108b33d13d7739712dea
Kubilay ADA <kubilayada.26@gmail.com>
2025-04-15 00:53:40 +0200

    inline onclick try for backbutton

--- backend_borrWow/src/SSRPage.js
+++ backend_borrWow/src/SSRPage.js
@@ -32,9 +32,7 @@ function SSRPage() {
           "button",
           {
             key: 4,
-            onClick: () => {
-              window.location.href = "https://borrwow2-1-jqmk.onrender.com/";
-            }, 
+            onclick: "window.location.href='https://borrwow2-1-jqmk.onrender.com/'",
             style: {
               marginTop: "20px",
               padding: "10px 20px",
